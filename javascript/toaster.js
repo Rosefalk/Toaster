ce16998114b6cc57282778bf173c4d143a252d45
Rosefalk <thomas_rosefalk@hotmail.com>
2015-07-07 11:35:18 +0200

    fixed bad nomenclature

--- javascript/toaster.js
+++ javascript/toaster.js
@@ -2,26 +2,26 @@ function toast(jsonData){
     //create reference to this without changing it
     var self = this;
     //create reference to jsonData without changing it
-    var json = $.extend({},self.defaults,jsonData);
+    var settings = $.extend({},self.defaults,jsonData);
     //toaster is the ID of the toaster you want to target
-    var $toaster= $("#"+json.toaster);
+    var $toaster= $("#"+settings.toaster);
     var $burner = $toaster.children(".burn");
     var $tmpl   = $toaster.children(".toast");
     var audio   = $toaster.children("audio").get(0);
     //clone the hidden template and insert data
     var $toast  = $tmpl.clone();
-        $toast.find(".title").text(json.title)
-        $toast.find(".text").text(json.text)
-        $toast.find(".left-column").css("background-image","url("+json.icon+")")
+        $toast.find(".title").text(settings.title)
+        $toast.find(".text").text(settings.text)
+        $toast.find(".left-column").css("background-image","url("+settings.icon+")")
     //if no icon has been chosen, remove it
-        if(json.icon === undefined){
+        if(settings.icon === undefined){
             $toast.find(".left-column").addClass("hide")
             $toast.find(".right-column").addClass("wide")
         }
     //if a function has been added, run it, if not, remove toast
-        if(json.func != undefined){
+        if(settings.func != undefined){
             $toast.on("click", function(){
-                json.func();
+                settings.func();
                 $(this).remove()
             })
         }else{
@@ -32,16 +32,19 @@ function toast(jsonData){
     //actuall instance the toast
         $toast.appendTo($burner);
     //optionally play sound
-        if(json.playSound){
+        if(settings.playSound){
             audio.play();
         }
     //remove toast after a while
-        $toast.fadeIn(400)
-              .delay(4000)
-              .fadeOut(1000, function() {
-                  $(this).remove();
-              })
-    //console.log(json)
+        $toast.fadeIn(settings.timeFadeIn)
+
+        if(settings.timeFadeOut != -1){
+            $toast.delay(settings.timeDelay)
+            .fadeOut(settings.timeFadeout, function() {
+              $(this).remove();
+            })
+        }
+    //console.log(settings)
     return self;
 }
 
@@ -53,7 +56,7 @@ var toaster = {
         text         : "Text",      //text of toaster
         timeFadeIn   : 400,         //time it takes to fade in
         timeDelay    : 4000,        //delay before it fades out
-        timeFadeOut  : 1000,        //toast fade out
+        timeFadeOut  : 4000,        //toast fade out
         func         : undefined,   //optional function to be run on click
         playSound    : true         //should a sound play when notifying?
     },
